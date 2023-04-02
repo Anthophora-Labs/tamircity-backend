7a5f7d1c4aba162287ec42338ed7430bb16b6807
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-04-02 13:48:44 +0300

    add accounts

---
+++ accounts/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model): #TechnicalService
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    description = models.TextField(blank=True, null=True, max_length=500)
+    twitter = models.CharField(max_length=200, blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-id"]
+
+    def __str__(self):
+        return self.user
+
+@receiver(post_save, sender=User)
+def create_user_prof(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
