ec2d997184653d010e636e6c7d265aad78e53f56
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-04-09 20:05:57 +0300

    upd expertise watch api

--- expertise_forms/api/urls.py
+++ expertise_forms/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from expertise_forms.api.views import (
                             ExpertiseTVCreateAPIView,
-                            ExpertiseWatchListAPIView,
+                            ExpertiseWatchCreateAPIView,
                         )
 
 app_name = 'expertise_forms'
@@ -9,5 +9,5 @@ app_name = 'expertise_forms'
 
 urlpatterns = [
     path('expertise-tv', ExpertiseTVCreateAPIView.as_view(), name='create_expertise_tv'),
-    path('expertise-watch', ExpertiseWatchListAPIView.as_view(), name='create_expertise_watch'),
+    path('expertise-watch', ExpertiseWatchCreateAPIView.as_view(), name='create_expertise_watch'),
 ]
