ee4d7a354674f8e94957331128cd0e0c8562ade1
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-04-14 21:42:54 +0300

    add configs for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tamircity_backend.wsgi
--- expertise_forms/admin.py
+++ expertise_forms/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ExpertiseTV, ExpertiseWatch
+from .models import ExpertiseTV, ExpertiseWatch, ExpertiseConsole, ExpertisePhone, ExpertisePc
 
 
 @admin.register(ExpertiseTV)
