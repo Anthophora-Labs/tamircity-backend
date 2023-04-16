1b102e07a37411831c44aff8f8977d36f3daa77a
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-04-16 18:06:12 +0300

    add tech service candidate

--- digital_onboarding/admin.py
+++ digital_onboarding/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import TechServiceCandidate
 
-# Register your models here.
+
+@admin.register(TechServiceCandidate)
+class TechServiceCandidateAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in TechServiceCandidate._meta.fields]
+    list_display_links = ["service_name"]
+    search_fields = ["service_name"]
+    list_filter = ["created_date"]
+
+    class Meta:
+        model = TechServiceCandidate
